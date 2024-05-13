5e1c9ed664ec7e3a867f398efc5aaaf3585dbd02
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-13 21:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let inName = document.querySelector("#fname");
+
+inName.addEventListener("change", upperCase);
+
+
+function upperCase(eventDetails) {
+let out = eventDetails.target; // Access the input field triggering the event
+  let newValue = out.value.toUpperCase();
+  out.value = newValue;
+}
